519f5a03975f03b32115c3cf09a16ebc155e00f3
Admin <Admin@DESKTOP-9H8GI7A>
2024-03-20 19:22:17 +0530

    First commit.......

---
+++ src/test/java/definitions/LoginPageDefinitions.java
@@ -0,0 +1,56 @@
+package definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import actions.HomePageActions;
+import actions.LoginPageActions;
+import utils.HelperClass;
+import org.testng.Assert;
+
+public class LoginPageDefinitions {
+
+	
+	LoginPageActions objLogin = new LoginPageActions();
+    HomePageActions objHomePage = new HomePageActions();
+ 
+    @Given("User is on HRMLogin page {string}")
+    public void loginTest(String url) {
+ 
+        HelperClass.openPage(url);
+ 
+    }
+ 
+    @When("User enters username as {string} and password as {string}")
+    public void goToHomePage(String userName, String passWord) {
+ 
+        // login to application
+        objLogin.login(userName, passWord);
+ 
+        // go the next page
+ 
+    }
+ 
+    @Then("User should be able to login successfully and new page open")
+    public void verifyLogin() {
+ 
+        // Verify home page
+        Assert.assertTrue(objHomePage.getHomePageText().contains("Dashboard"));
+ 
+    }
+ 
+    @Then("User should be able to see error message {string}")
+    public void verifyErrorMessage(String expectedErrorMessage) {
+ 
+        // Verify error message
+        Assert.assertEquals(objLogin.getErrorMessage(),expectedErrorMessage);
+ 
+    }
+ 
+    @Then("User should be able to see a message {string} below Username")
+    public void verifyMissingUsernameMessage(String message) {
+ 
+        Assert.assertEquals(objLogin.getMissingUsernameText(),message);
+    }
+	
+}
